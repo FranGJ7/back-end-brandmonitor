c26a2fd138ca41851073294a7f115a6127edff01
Fransérgio Gonçalves Júnior <fransergiogj7@gmail.com>
2023-01-12 20:06:16 -0300

    Model de usúarios criado - instalação de dependência Bcrypt

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+//configurações iniciais
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+//import dos models
+const Person = require('./models/Person')
+
+//Metodo de leitura do json
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+
+//rotas
+app.get('/', (req, res)=>{
+
+
+   res.json({message: 'Oi'})
+})
+
+
+
+
+//Porta entregue
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://brand-monitor:JKlDbMPpPAUPlSaz@cluster0.q2wdxwg.mongodb.net/?retryWrites=true&w=majority')
+.then(() =>{ console.log('Connection succesfull')
+ app.listen(3000)})
+.catch((error) => console.log(error," Erro na conexão com Banco de dados"));
 
---
+++ models/Person.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+
+const PersonSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    gender: {
+        type: String,
+        enum: ['masculino', 'feminino', 'não informado']
+    }
+})
+
+PersonSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10)
+    }
+    next()
+})
+
+const Person = mongoose.model('Person', PersonSchema)
+module.exports = Person
